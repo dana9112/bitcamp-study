4a40b23c701845c0f411d0c29510517e72c6b3fb
DongYoungSon <ehddud2978@gmail.com>
2019-12-06 17:59:27 +0900

    데이터 종류에 따른 리터럴 표기법

--- bitcamp-java/src/main/java/com/ohoracs/basic/ex03/Exam1.java
+++ bitcamp-java/src/main/java/com/ohoracs/basic/ex03/Exam1.java
@@ -1,5 +1,6 @@
 package com.ohoracs.basic.ex03;
 
+
 public class Exam1 {
   public static void main(String[] args) {
     //정수 리터럴
@@ -16,6 +17,29 @@ public class Exam1 {
     System.out.println(0x4e); //16진수
     System.out.println(-0x4e); //16진수
 
-    System.out.println(0b01001110); //16진수
+    System.out.println(0b01001110); //8진수
+    
+    //부동소수점 리터럴
+    System.out.println(3.14); //고정소수점
+    System.out.println(0.314e1 /*  *10*/ );
+    System.out.println(31.4e-1);
+
+    //논리 리터럴
+    System.out.println(true);
+    System.out.println(false);
+
+    //문자 리터럴
+    System.out.println('A');
+    System.out.println('가');
+
+    //문자열 리터럴
+    System.out.println("안녕하세요! 비트캠프!");
+    
+
+
+
+
+
+
   }
 }
