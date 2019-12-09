47d5e274c88606db3b3386d9775d87bfa80e5d95
DongYoungSon <ehddud2978@gmail.com>
2019-12-09 17:28:16 +0900

    리터럴에 대해서

--- bitcamp-java/src/main/java/com/eomcs/basic/ex03/Exam42.java
+++ bitcamp-java/src/main/java/com/eomcs/basic/ex03/Exam42.java
@@ -17,5 +17,11 @@ public class Exam42 {
     System.out.println((char)0x0041); //앞에 00은 생략 가능
     System.out.println((char)0xac00);
 
+    for (int i = 0; i < 11172; i++){
+      if (i % 80 == 0)
+        System.out.println();
+      System.out.print((char)(0xac00 + i));
+    }
+
   }
 }
