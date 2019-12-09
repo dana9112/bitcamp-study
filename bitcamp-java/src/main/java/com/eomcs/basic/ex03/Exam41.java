267d5c1595a7fd070f1c0e470b664712dba0a4ba
DongYoungSon <ehddud2978@gmail.com>
2019-12-09 17:09:21 +0900

    리터럴에 대해서

--- bitcamp-java/src/main/java/com/eomcs/basic/ex03/Exam41.java
+++ bitcamp-java/src/main/java/com/eomcs/basic/ex03/Exam41.java
@@ -5,11 +5,18 @@ package com.eomcs.basic.ex03;
 public class Exam41 {
   public static void main(String[] args) {
 
+    // 직접문자를 작성할 수 있다.
+    // => [문자]
     System.out.println('A');
     System.out.println('가');
+
+    // UCS-2 코드 값으로 문자를 지정할 수 있다.
+    // => [\ u][UCS-2 코드]
     System.out.println('\u4eba'); // = 人과 동일
-    
-    
+    System.out.println('\u0041'); // A문자
+    System.out.println('\u0030'); // 0문자
+    System.out.println('\u0025'); // %문자
+    System.out.println('\uac00'); // 가 문자
 
     
   }
