fc1a266f284ebd4c6ecae2bf07262cf0cbf43b69
DongYoungSon <ehddud2978@gmail.com>
2020-01-23 18:08:44 +0900

    2020-01-23

---
+++ bitcamp-project/v28_3/src/main/java/com/eomcs/lms/handler/LessonListCommand.java
@@ -0,0 +1,32 @@
+package com.eomcs.lms.handler;
+
+import java.util.Iterator;
+import java.util.List;
+import com.eomcs.lms.domain.Lesson;
+import com.eomcs.util.Prompt;
+
+public class LessonListCommand implements Command {
+  List<Lesson> lessonList;
+
+  Prompt prompt;
+
+  public LessonListCommand(List<Lesson> list) {
+    this.lessonList = list;
+
+  }
+
+  @Override
+  public void execute() {
+    Iterator<Lesson> iterator = lessonList.iterator();
+    while (iterator.hasNext()) {
+      Lesson l = iterator.next();
+      System.out.printf("%d, %s, %s ~ %s, %d\n", l.getNo(), l.getTitle(), l.getStartDate(),
+          l.getEndDate(), l.getTotalHours());
+    }
+  }
+
+
+
+}
+
+
