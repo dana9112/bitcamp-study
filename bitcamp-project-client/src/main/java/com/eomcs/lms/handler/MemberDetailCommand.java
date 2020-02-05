457e0f7c3b52392edc3aa2ba4bbf9e5e0166559f
DongYoungSon <ehddud2978@gmail.com>
2020-02-05 10:24:07 +0900

    2020-02-05

--- bitcamp-project-client/src/main/java/com/eomcs/lms/handler/MemberDetailCommand.java
+++ bitcamp-project-client/src/main/java/com/eomcs/lms/handler/MemberDetailCommand.java
@@ -2,16 +2,17 @@ package com.eomcs.lms.handler;
 
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import com.eomcs.lms.domain.Member;
 import com.eomcs.util.Prompt;
 
-public class LessonDeleteCommand implements Command {
+public class MemberDetailCommand implements Command {
 
   ObjectOutputStream out;
   ObjectInputStream in;
 
   Prompt prompt;
 
-  public LessonDeleteCommand(ObjectOutputStream out, ObjectInputStream in, Prompt prompt) {
+  public MemberDetailCommand(ObjectOutputStream out, ObjectInputStream in, Prompt prompt) {
     this.out = out;
     this.in = in;
     this.prompt = prompt;
@@ -19,11 +20,10 @@ public class LessonDeleteCommand implements Command {
 
   @Override
   public void execute() {
-
     try {
       int no = prompt.inputInt("번호? ");
 
-      out.writeUTF("/lesson/delete");
+      out.writeUTF("/member/detail");
       out.writeInt(no);
       out.flush();
 
@@ -33,12 +33,18 @@ public class LessonDeleteCommand implements Command {
         System.out.println(in.readUTF());
         return;
       }
-      System.out.println("수업을 삭제했습니다.");
+
+      Member member = (Member) in.readObject();
+      System.out.printf("번호: %d\n", member.getNo());
+      System.out.printf("이름: %s\n", member.getName());
+      System.out.printf("이메일: %s\n", member.getEmail());
+      System.out.printf("암호: %s\n", member.getPassword());
+      System.out.printf("사진: %s\n", member.getPhoto());
+      System.out.printf("전화: %s\n", member.getTel());
     } catch (Exception e) {
       System.out.println("명령 실행 중 오류 발생!");
     }
   }
 
+
 }
-
-
--- bitcamp-project-client/v32_4/src/main/java/com/eomcs/lms/handler/MemberDeleteCommand.java
+++ bitcamp-project-client/v32_4/src/main/java/com/eomcs/lms/handler/MemberDeleteCommand.java
@@ -4,14 +4,14 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import com.eomcs.util.Prompt;
 
-public class LessonDeleteCommand implements Command {
+public class MemberDeleteCommand implements Command {
 
   ObjectOutputStream out;
   ObjectInputStream in;
 
   Prompt prompt;
 
-  public LessonDeleteCommand(ObjectOutputStream out, ObjectInputStream in, Prompt prompt) {
+  public MemberDeleteCommand(ObjectOutputStream out, ObjectInputStream in, Prompt prompt) {
     this.out = out;
     this.in = in;
     this.prompt = prompt;
@@ -19,11 +19,10 @@ public class LessonDeleteCommand implements Command {
 
   @Override
   public void execute() {
-
     try {
       int no = prompt.inputInt("번호? ");
 
-      out.writeUTF("/lesson/delete");
+      out.writeUTF("/member/delete");
       out.writeInt(no);
       out.flush();
 
@@ -33,12 +32,10 @@ public class LessonDeleteCommand implements Command {
         System.out.println(in.readUTF());
         return;
       }
-      System.out.println("수업을 삭제했습니다.");
+      System.out.println("게시글을 삭제했습니다.");
+
     } catch (Exception e) {
       System.out.println("명령 실행 중 오류 발생!");
     }
   }
-
 }
-
-
