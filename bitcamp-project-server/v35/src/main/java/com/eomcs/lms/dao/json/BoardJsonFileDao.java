4891af167aa6b83cb173cff0fa0e04f8f1dc2dc4
DongYoungSon <ehddud2978@gmail.com>
2020-02-14 12:59:56 +0900

    2020-02-14

---
+++ bitcamp-project-server/v35/src/main/java/com/eomcs/lms/dao/json/BoardJsonFileDao.java
@@ -0,0 +1,72 @@
+package com.eomcs.lms.dao.json;
+
+import java.util.List;
+import com.eomcs.lms.dao.BoardDao;
+import com.eomcs.lms.domain.Board;
+
+public class BoardJsonFileDao extends AbstractJsonFileDao<Board> implements BoardDao { // 6.
+                                                                                       // AbstractObjectFileDao
+  // 상속 받기
+
+  public BoardJsonFileDao(String filename) {
+    super(filename);
+  }
+
+
+  // 서블릿 객체들이 데이터를 다룰 때 사용할 메서드를 정의한다.
+  public int insert(Board board) throws Exception { // 인서트의 리턴타입은 인트이다. 인서트가 안되면 0을 리턴한다.
+
+    if (indexOf(board.getNo()) > -1) { // 같은 번호의 게시물이 있다면,
+      return 0;
+    }
+    list.add(board); // 새 게시물을 등록한다.
+    saveData(); // 데이터가 세이브 될때마다 저장한다.
+    return 1;
+  }
+
+  public List<Board> findAll() throws Exception { // 보드 객체의 리스트를 달라.
+    return list;
+  }
+
+  public Board findByNo(int no) throws Exception {
+    int index = indexOf(no);
+    if (index == -1) {
+      return null;
+    }
+    return list.get(index);
+  }
+
+  public int update(Board board) throws Exception {
+    int index = indexOf(board.getNo());
+    if (index == -1) {
+      return 0;
+    }
+    list.set(index, board); // 기존 객체를 파라미터로 받은 객체로 바꾼다.
+    saveData();
+    return 1;
+  }
+
+  public int delete(int no) throws Exception {
+    int index = indexOf(no);
+    if (index == -1) {
+      return 0;
+    }
+
+    list.remove(index);
+    saveData();
+    return 1;
+
+  }
+
+  @Override
+  protected <K> int indexOf(K key) {
+    for (int i = 0; i < list.size(); i++) {
+      if (list.get(i).getNo() == (int) key) { // K가 어떤 타입인지 형변환해서 사용하라.
+        // 번호를 키 => 인트라고 타입케스팅 해주기
+        return i;
+      }
+    }
+    return -1;
+  }
+
+}
---
+++ bitcamp-project-server/v35/src/main/java/com/eomcs/lms/servlet/LessonAddServlet.java
@@ -0,0 +1,34 @@
+package com.eomcs.lms.servlet;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import com.eomcs.lms.dao.LessonDao;
+import com.eomcs.lms.domain.Lesson;
+
+public class LessonAddServlet implements Servlet {
+
+  LessonDao lessonDao;
+
+  public LessonAddServlet(LessonDao lessonDao) {
+    this.lessonDao = lessonDao;
+  }
+
+
+  // 커맨드 패턴이란 메소드를 객
+  @Override
+  public void service(ObjectInputStream in, ObjectOutputStream out) throws Exception {
+
+    Lesson lesson = (Lesson) in.readObject();
+
+
+    if (lessonDao.insert(lesson) > 0) {
+      out.writeUTF("OK");
+
+    } else {
+      out.writeUTF("FAIL");
+      out.writeUTF("같은 번호의 수업이 있습니다.");
+    }
+
+  }
+
+}
