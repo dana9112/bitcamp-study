f8a74533722e3dcd9fcdcd5143052384b5d8dec4
DongYoungSon <ehddud2978@gmail.com>
2020-02-21 12:08:59 +0900

    2020-02-21

--- bitcamp-java/src/main/java/com/eomcs/jdbc/ex2/test/Exam0130.java
+++ bitcamp-java/src/main/java/com/eomcs/jdbc/ex2/test/Exam0130.java
@@ -31,7 +31,7 @@ public class Exam0130 {
     try (Connection con = DriverManager.getConnection(//
         "jdbc:mysql://localhost:3306/studydb?user=study&password=1111");
         PreparedStatement stmt = con.prepareStatement(//
-            "select * from x_board order by board_id = ?");) {
+            "select * from x_board order by board_id = ?")) {
 
       stmt.setString(1, no);
 
