f722bfdf960468ec5c2ccac31b6594d4e4c8e4e9
DongYoungSon <ehddud2978@gmail.com>
2020-02-28 17:21:44 +0900

    2020-02-28

--- bitcamp-java/src/main/java/com/eomcs/mybatis/ex02/Exam0211.java
+++ bitcamp-java/src/main/java/com/eomcs/mybatis/ex02/Exam0211.java
@@ -1,4 +1,4 @@
-// Mybatis - 자바 객체의 프로퍼티 이름과 컬럼명을 일치시키기
+// Mybatis - SQL에 파라미터 지정하기: String 값 넘겨주기
 package com.eomcs.mybatis.ex02;
 
 import java.io.InputStream;
@@ -8,16 +8,22 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-public class Exam0120 {
+public class Exam0211 {
 
   public static void main(String[] args) throws Exception {
     InputStream inputStream =
-        Resources.getResourceAsStream("com/eomcs/mybatis/ex02/mybatis-config02.xml");
+        Resources.getResourceAsStream("com/eomcs/mybatis/ex02/mybatis-config04.xml");
     SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
 
     SqlSession sqlSession = factory.openSession();
 
-    List<Board> list = sqlSession.selectList("BoardMapper.selectBoard");
+    // SQL을 실행할 때 파라미터 값을 전달하려면
+    // 두 번째 파라미터로 전달해야 한다.
+    // 여러 개의 값을 전달해야 한다면,
+    // Map 객체에 담아 전달하라!
+
+    // 예) 게시글 제목에 오호라를 포함한 모든 게시글을 찾는다.
+    List<Board> list = sqlSession.selectList("BoardMapper.selectBoard2", "%오호라%");
 
     for (Board board : list) {
       System.out.printf("%d, %s, %s, %s, %d\n", board.getNo(), board.getTitle(), board.getContent(),
--- bitcamp-java/src/main/java/com/eomcs/mybatis/ex02/Exam0221.java
+++ bitcamp-java/src/main/java/com/eomcs/mybatis/ex02/Exam0221.java
@@ -1,4 +1,4 @@
-// Mybatis - 자바 객체의 프로퍼티 이름과 컬럼명을 일치시키기
+// Mybatis - SQL에 파라미터 지정하기 : #{} 의 한계
 package com.eomcs.mybatis.ex02;
 
 import java.io.InputStream;
@@ -8,16 +8,19 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-public class Exam0120 {
+public class Exam0221 {
 
   public static void main(String[] args) throws Exception {
     InputStream inputStream =
-        Resources.getResourceAsStream("com/eomcs/mybatis/ex02/mybatis-config02.xml");
+        Resources.getResourceAsStream("com/eomcs/mybatis/ex02/mybatis-config05.xml");
     SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
 
     SqlSession sqlSession = factory.openSession();
 
-    List<Board> list = sqlSession.selectList("BoardMapper.selectBoard");
+    // 예) 파라미터로 컬럼 이름을 넘겨주면
+    // 해당 컬럼의 값을 오름차순으로 정렬한다.
+    List<Board> list = sqlSession.selectList("BoardMapper.selectBoard2", "title");
+    // ${} 문법은 값을 SQL 문에 그대로 삽입한다.
 
     for (Board board : list) {
       System.out.printf("%d, %s, %s, %s, %d\n", board.getNo(), board.getTitle(), board.getContent(),
