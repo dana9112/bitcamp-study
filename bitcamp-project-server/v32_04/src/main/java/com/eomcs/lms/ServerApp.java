7d8356ac27456fe598213cd89e8d9e29df4ac927
DongYoungSon <ehddud2978@gmail.com>
2020-03-17 18:21:48 +0900

    2020-03-17

--- bitcamp-project-server/v32_04/src/main/java/com/eomcs/lms/ServerApp.java
+++ bitcamp-project-server/v32_04/src/main/java/com/eomcs/lms/ServerApp.java
@@ -98,8 +98,8 @@ public class ServerApp {
         }
 
         List<Board> boards = (List<Board>) context.get("boardList");
-        List<Lesson> lessons = (List<Lesson>) context.get("lessonList");
         List<Member> members = (List<Member>) context.get("memberList");
+        List<Lesson> lessons = (List<Lesson>) context.get("lessonList");
 
         if (request.equals("/board/list")) {
           out.writeUTF("OK");
@@ -209,110 +209,6 @@ public class ServerApp {
             out.writeUTF(e.getMessage());
           }
 
-        } else if (request.equals("/lesson/list")) {
-          out.writeUTF("OK");
-          out.reset();
-          out.writeObject(lessons);
-
-        } else if (request.equals("/lesson/add")) {
-          try {
-            Lesson lesson = (Lesson) in.readObject();
-
-            int i = 0;
-            for (; i < lessons.size(); i++) {
-              if (lessons.get(i).getNo() == lesson.getNo()) {
-                break;
-              }
-            }
-
-            if (i == lessons.size()) {
-              lessons.add(lesson);
-              out.writeUTF("OK");
-            } else {
-              out.writeUTF("FAIL");
-              out.writeUTF("같은 번호의 게시물이 있습니다.");
-            }
-
-          } catch (Exception e) {
-            out.writeUTF("FAIL");
-            out.writeUTF(e.getMessage());
-
-          }
-        } else if (request.equals("/lesson/detail")) {
-          try {
-            int no = in.readInt();
-
-            Lesson lesson = null;
-            for (Lesson l : lessons) {
-              if (l.getNo() == no) {
-                lesson = l;
-                break;
-              }
-            }
-
-            if (lesson != null) {
-              out.writeUTF("OK");
-              out.writeObject(lesson);
-            } else {
-              out.writeUTF("Fail");
-              out.writeUTF("해당 번호의 게시물이 없습니다.");
-            }
-
-          } catch (Exception e) {
-            out.writeUTF("FAIL");
-            out.writeObject(e.getMessage());
-
-          } // catch end ()
-
-        } else if (request.equals("/lesson/update")) {
-          try {
-            Lesson lesson = (Lesson) in.readObject();
-
-            int index = -1;
-            for (int i = 0; i < lessons.size(); i++) {
-              if (lessons.get(i).getNo() == lesson.getNo()) {
-                index = i;
-                break;
-              }
-            }
-
-            if (index != -1) {
-              lessons.set(index, lesson);
-              out.writeUTF("OK");
-            } else {
-              out.writeUTF("FAIL");
-              out.writeUTF("해당 번호의 게시물이 없습니다.");
-            }
-
-          } catch (Exception e) {
-            out.writeUTF("FAIL");
-            out.writeUTF(e.getMessage());
-          }
-        } else if (request.equals("/lesson/delete")) {
-          try {
-            int no = in.readInt();
-
-            int index = -1;
-            for (int i = 0; i < lessons.size(); i++) {
-              if (lessons.get(i).getNo() == no) {
-                index = i;
-                break;
-              }
-            }
-
-            if (index != -1) { // 삭제하려는 번호의 게시물을 찾았다면
-              lessons.remove(index);
-              out.writeUTF("OK");
-
-            } else {
-              out.writeUTF("FAIL");
-              out.writeUTF("해당 번호의 게시물이 없습니다.");
-            }
-          } catch (Exception e) {
-            out.writeUTF("FAIL");
-            out.writeUTF(e.getMessage());
-          }
-
         } else if (request.equals("/member/list")) {
           out.writeUTF("OK");
           out.reset();
@@ -335,7 +231,7 @@ public class ServerApp {
 
             } else {
               out.writeUTF("FAIL");
-              out.writeUTF("같은 번호의 게시물이 있습니다.");
+              out.writeUTF("같은 번호의 회원이 있습니다.");
             }
 
 
@@ -361,7 +257,7 @@ public class ServerApp {
 
             } else {
               out.writeUTF("FAIL");
-              out.writeUTF("해당 번호의 게시물이 없습니다.");
+              out.writeUTF("해당 번호의 회원이 없습니다.");
             }
 
           } catch (Exception e) {
@@ -385,7 +281,7 @@ public class ServerApp {
               out.writeUTF("OK");
             } else {
               out.writeUTF("FAIL");
-              out.writeUTF("해당 번호의 게시물이 없습니다.");
+              out.writeUTF("해당 번호의 회원이 없습니다.");
             }
 
           } catch (Exception e) {
@@ -404,13 +300,117 @@ public class ServerApp {
               }
             }
 
-            if (index != -1) { // 삭제하려는 번호의 게시물을 찾았다면
+            if (index != -1) {
               members.remove(index);
               out.writeUTF("OK");
 
             } else {
               out.writeUTF("FAIL");
-              out.writeUTF("해당 번호의 게시물이 없습니다.");
+              out.writeUTF("해당 번호의 회원이 없습니다.");
+            }
+          } catch (Exception e) {
+            out.writeUTF("FAIL");
+            out.writeUTF(e.getMessage());
+          }
+
+        } else if (request.equals("/lesson/list")) {
+          out.writeUTF("OK");
+          out.reset();
+          out.writeObject(lessons);
+
+        } else if (request.equals("/lesson/add")) {
+          try {
+            Lesson lesson = (Lesson) in.readObject();
+
+            int i = 0;
+            for (; i < lessons.size(); i++) {
+              if (lessons.get(i).getNo() == lesson.getNo()) {
+                break;
+              }
+            }
+
+            if (i == lessons.size()) {
+              lessons.add(lesson);
+              out.writeUTF("OK");
+
+            } else {
+              out.writeUTF("FAIL");
+              out.writeUTF("같은 번호의 수업이 있습니다.");
+            }
+
+
+          } catch (Exception e) {
+            out.writeUTF("FAIL");
+            out.writeUTF(e.getMessage());
+          }
+        } else if (request.equals("/lesson/detail")) {
+          try {
+            int no = in.readInt();
+
+            Lesson lesson = null;
+            for (Lesson l : lessons) {
+              if (l.getNo() == no) {
+                lesson = l;
+                break;
+              }
+            }
+
+            if (lesson != null) {
+              out.writeUTF("OK");
+              out.writeObject(lesson);
+
+            } else {
+              out.writeUTF("FAIL");
+              out.writeUTF("해당 번호의 수업이 없습니다.");
+            }
+
+          } catch (Exception e) {
+            out.writeUTF("FAIL");
+            out.writeUTF(e.getMessage());
+          }
+        } else if (request.equals("/lesson/update")) {
+          try {
+            Lesson lesson = (Lesson) in.readObject();
+
+            int index = -1;
+            for (int i = 0; i < lessons.size(); i++) {
+              if (lessons.get(i).getNo() == lesson.getNo()) {
+                index = i;
+                break;
+              }
+            }
+
+            if (index != -1) {
+              lessons.set(index, lesson);
+              out.writeUTF("OK");
+            } else {
+              out.writeUTF("FAIL");
+              out.writeUTF("해당 번호의 수업이 없습니다.");
+            }
+
+          } catch (Exception e) {
+            out.writeUTF("FAIL");
+            out.writeUTF(e.getMessage());
+          }
+        } else if (request.equals("/lesson/delete")) {
+          try {
+            int no = in.readInt();
+
+            int index = -1;
+            for (int i = 0; i < lessons.size(); i++) {
+              if (lessons.get(i).getNo() == no) {
+                index = i;
+                break;
+              }
+            }
+
+            if (index != -1) {
+              lessons.remove(index);
+              out.writeUTF("OK");
+
+            } else {
+              out.writeUTF("FAIL");
+              out.writeUTF("해당 번호의 수업이 없습니다.");
             }
           } catch (Exception e) {
             out.writeUTF("FAIL");
